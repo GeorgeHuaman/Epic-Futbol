3fd5d0ba782f3911b9a82ac1add4d1692ac68288
George Jimy <knivespeans@gmail.com>
2024-11-07 20:48:07 -0500

    patear pelota

---
+++ Assets/Scripts/KickBall.cs
@@ -0,0 +1,30 @@
+using SpatialSys.UnitySDK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickBall : MonoBehaviour
+{
+    public float cooldown = 0.2f; // Tiempo de espera entre patadas en segundos
+    private float time;
+    public Transform ballTransform;
+    public Rigidbody ballRb;
+    public float kickForce;
+
+
+    void Update()
+    {
+        Vector3 playerPosition = SpatialBridge.actorService.localActor.avatar.position;
+        playerPosition.y -= 0.3f; // Ajusta el valor según lo necesario
+        Vector3 direction = (ballTransform.position - playerPosition).normalized;
+        float distance = Vector3.Distance(ballTransform.position, playerPosition);
+
+        time += Time.deltaTime;
+
+        if (distance < 1.25f && time >= cooldown)
+        {
+            ballRb.AddForce(direction * kickForce, ForceMode.Impulse);
+            time = 0f;
+        }
+    }
+}
